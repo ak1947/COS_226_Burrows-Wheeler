d04fa5f2f52dc5d72eedc298aa158cb75f5ba92c
Aadarsh Saxena <adarshtjc@gmail.com>
2013-12-29 18:01:02 +0800

    Flushing stdio.

--- src/CircularSuffixArray.java
+++ src/CircularSuffixArray.java
@@ -1,18 +1,19 @@
 public class CircularSuffixArray {
+    private int[] index;
+
     public CircularSuffixArray(String s)  // circular suffix array of s
     {
+        index = new int[s.length()];
 
     }
 
     public int length()                   // length of s
     {
-        return 0;
-
+        return index.length;
     }
 
     public int index(int i)               // returns index of ith sorted suffix
     {
-        return 0;
-
+        return index[i];
     }
 }
--- src/MoveToFront.java
+++ src/MoveToFront.java
@@ -6,30 +6,37 @@ public class MoveToFront {
         for (int i = 0; i < 256; i++)
             charAtIndex[i] = i;
 
-        while (!BinaryStdIn.isEmpty()) {
-            char readChar = BinaryStdIn.readChar(8);
+        String s = BinaryStdIn.readString();
+        char[] input = s.toCharArray();
+
+        for (int i = 0; i < input.length; i++) {
+            char readChar = input[i];
             int currentCharIndex = 0;
             while (charAtIndex[currentCharIndex] != readChar)
                 currentCharIndex++;
             BinaryStdOut.write(currentCharIndex, 8);
             // move readChar to front! increase index of the rest of characters
-            for (int i = currentCharIndex; i > 0; i--)
-                charAtIndex[i] = charAtIndex[i - 1];
+            for (int j = currentCharIndex; j > 0; j--)
+                charAtIndex[j] = charAtIndex[j - 1];
             charAtIndex[0] = readChar;
         }
+
+        BinaryStdOut.flush();
     }
 
     // apply move-to-front decoding, reading from standard input and writing to standard output
     public static void decode() {
 
-        int charAtBeginning = 0;
+        String s = BinaryStdIn.readString();
+        char[] input = s.toCharArray();
+
         int[] charAtIndex = new int[256];
 
         for (int i = 0; i < 256; i++)
             charAtIndex[i] = i;
 
-        while (!BinaryStdIn.isEmpty()) {
-            int curCharIndex = BinaryStdIn.readChar(8);
+        for (int j = 0; j < input.length; j++) {
+            int curCharIndex = input[j];
             int curChar = charAtIndex[curCharIndex];
             BinaryStdOut.write(curChar, 8);
 
@@ -37,6 +44,8 @@ public class MoveToFront {
                 charAtIndex[i] = charAtIndex[i - 1];
             charAtIndex[0] = curChar;
         }
+
+        BinaryStdOut.flush();
 
     }
 
