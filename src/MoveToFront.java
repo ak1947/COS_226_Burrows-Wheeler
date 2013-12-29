70b22fe7908f041937d8f4bc94ba5b7e02ca1463
Aadarsh Saxena <adarshtjc@gmail.com>
2013-12-29 23:58:33 +0800

    Linked List implementation of MoveToFront. Timing performance is worse.

--- src/MoveToFront.java
+++ src/MoveToFront.java
@@ -1,21 +1,23 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+
 public class MoveToFront {
     // apply move-to-front encoding, reading from standard input and writing to standard output
     public static void encode() {
-        int[] charAtIndex = new int[256];
+        LinkedList linkedList = new LinkedList();
 
-        for (int i = 0; i < 256; i++)
-            charAtIndex[i] = i;
+        for (char i = 0; i < 256; i++){
+            linkedList.add(i);
+        }
 
         while (!BinaryStdIn.isEmpty()) {
             char readChar = BinaryStdIn.readChar(8);
-            int currentCharIndex = 0;
-            while (charAtIndex[currentCharIndex] != readChar)
-                currentCharIndex++;
+            int currentCharIndex = linkedList.indexOf(readChar);
             BinaryStdOut.write(currentCharIndex, 8);
             // move readChar to front! increase index of the rest of characters
-            for (int j = currentCharIndex; j > 0; j--)
-                charAtIndex[j] = charAtIndex[j - 1];
-            charAtIndex[0] = readChar;
+            linkedList.remove(currentCharIndex);
+            linkedList.add(0, readChar);
         }
 
         BinaryStdOut.flush();
@@ -44,7 +46,8 @@ public class MoveToFront {
 
     // if args[0] is '-', apply move-to-front encoding
     // if args[0] is '+', apply move-to-front decoding
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+//        System.setIn(new FileInputStream("abra.txt"));
         if (args[0].equals("-")) encode();
         else if (args[0].equals("+")) decode();
         else throw new IllegalArgumentException("Illegal command line argument");
